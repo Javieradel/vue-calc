873aec806e84e99e43164039126c8fc8418f64b8
javieradel <ashair455@gmail.com>
2021-09-15 19:24:26 -0400

    make store

---
+++ src/store.js
@@ -0,0 +1,35 @@
+import {createStore} from 'vuex'
+
+const store = createStore({
+  state: {
+    input: '',
+    operation: 'none',
+    history: [],
+    resolve: false,
+    result:''
+  },
+  mutations: {
+    SET_INPUT(state ,payload){
+      state.input +=payload
+    },
+    SET_OPERATOR(state,payload){
+      state.operation= payload
+      state.input+= payload
+    },
+    DEL(state){
+      state.input = state.input.slice(0, state.input.length-1)
+    },
+    SET_RESULT(state){
+      state.result= eval(state.input)
+    }
+  },
+  actions: {
+  },
+  getters: {
+    currentResult(state){
+      return eval(state.input)
+    }
+  }
+
+})
+export default store
