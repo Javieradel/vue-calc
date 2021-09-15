454533544ccf862922da026a032ce41a4c0b052a
javieradel <ashair455@gmail.com>
2021-09-15 19:23:53 -0400

    key component

---
+++ src/components/key.vue
@@ -0,0 +1,35 @@
+<template>
+  
+  <li>
+    <span>{{val}}</span>
+  </li>
+
+</template>
+<script>
+import {defineComponent } from 'vue'
+
+export default defineComponent({
+    
+props: {
+ val: {
+    type: [String,Number],
+    default: ''
+  }
+  }
+
+  })
+</script>
+
+<style lang="scss" scoped>
+  li{
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: calc(100%/3);
+    height: 130px;
+    background: #2d2d2f;
+    margin: 0 !important;
+    color: whitesmoke;
+    font-size: 2rem;
+  }
+</style>
